73036e393b3fee5111c582f06fcf089a1086acfd
root <root@localhost>
2018-02-12 15:15:18 +0100

    test kube

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -13,6 +13,6 @@ public class Greeting {
 
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public @ResponseBody String greeting(@PathVariable(value = "name") String name) {
-		return "Hello Meetup Test 3 " + name;
+		return "Hello Kube " + name;
 	}
 }
